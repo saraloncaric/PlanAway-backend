42b651db6b22eb5b7b05d253df4abb2503933b3e
saraloncaric <167783165+saraloncaric@users.noreply.github.com>
2026-02-23 14:11:10 +0100

    promjene zbog deploy greske baze

--- db.js
+++ db.js
@@ -5,11 +5,7 @@ dotenv.config()
 const { Pool } = pg
 
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 })
 pool.connect((error, client, release) => {
